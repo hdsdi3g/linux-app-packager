0e898af9ec88c5823efffdfd88820d065868556f
hdsdi3g <admin@hd3g.tv>
2020-12-14 10:10:53 +0100

    Change the return script package name by PACKAGENAME

--- package-all.bash
+++ package-all.bash
@@ -13,5 +13,5 @@ PACKAGEDIR=$(pwd)/packages;
 echo "You will found packages in $PACKAGEDIR directory";
 echo "They are now ready to deploy on Linux/WSL host";
 echo "";
-echo "Usage for a simple autoextract test/check: apackage-run.sh --keep --target \"extracted\" [-- -norun]";
-echo "Usage for deploy to another root directory: apackage-run.sh [-- -chroot \"/opt\"]";
+echo "Usage for a simple autoextract test/check: PACKAGENAME-run.sh --keep --target \"extracted\" [-- -norun]";
+echo "Usage for deploy to another root directory: PACKAGENAME-run.sh [-- -chroot \"/opt\"]";
